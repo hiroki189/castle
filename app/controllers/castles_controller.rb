769b8e4d01ab9f002492ee2475e1bae6174f6f35
hiroki189 <hiroki18950@icloud.com>
2019-07-22 15:51:04 +0900

    create_user_funtion

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -11,6 +11,6 @@ class CastlesController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index 
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190717080315) do
+ActiveRecord::Schema.define(version: 20190722061534) do
 
   create_table "citadels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20190717080315) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -33,8 +32,8 @@ ActiveRecord::Schema.define(version: 20190717080315) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
