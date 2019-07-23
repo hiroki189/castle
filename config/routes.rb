d39c0bc3048c82ff832863a41aa0bf3b601b38df
hiroki189 <hiroki18950@icloud.com>
2019-07-23 13:46:56 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :twopage, only: [:index]
   resources :threepage, only: [:index]
 
-  resources :comments, only: [:new]
+  resources :citadels do
+    resources :comments, only: [:create]
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
