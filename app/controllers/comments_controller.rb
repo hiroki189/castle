9d8e9bcb703556bac15653a62fe757f8125a6fef
hiroki189 <hiroki18950@icloud.com>
2019-07-23 18:40:40 +0900

    form_function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
-  def new
+  def create
+    comment = Comment.create(text: comment_params[:text],citadel_id: comment_params[:citadel_id], user_id: current_user.id)
+    redirect_to "/castles/#{comment.citadel_id}"
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :citadel_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    user = User.find(params[:id])
     @nickname = current_user.nickname
     @citadels = Citadel.where(user_id: current_user.id)
     
