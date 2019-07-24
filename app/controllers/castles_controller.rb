e9882a43d62c7d31cef2def03aa9bd79c61c2df5
hiroki189 <hiroki18950@icloud.com>
2019-07-24 12:21:01 +0900

    modify

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -23,8 +23,6 @@ class CastlesController < ApplicationController
     params.permit(:name, :image, :text)
   end
 
-
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
   end
 end
