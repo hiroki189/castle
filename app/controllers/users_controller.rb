fec415474579da74022a870e493306a465273e8c
hiroki189 <hiroki18950@icloud.com>
2019-07-24 12:23:14 +0900

    modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,5 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = current_user.nickname
     @citadels = Citadel.where(user_id: current_user.id)
-    
   end
 end
