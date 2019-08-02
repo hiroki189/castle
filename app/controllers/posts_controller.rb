f2f28ca3112602f58d2867cebe82529569e41bf5
hiroki189 <hiroki18950@icloud.com>
2019-08-02 15:27:06 +0900

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,21 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+end
 end
