bbdba401e8502f51b00348d0d548f3cf2b1f4ac8
hiroki189 <hiroki18950@icloud.com>
2019-08-06 10:47:46 +0900

    create_comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
   def show
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
     @nickname = current_user.nickname
     @citadels = Citadel.where(user_id: current_user.id)
   end
